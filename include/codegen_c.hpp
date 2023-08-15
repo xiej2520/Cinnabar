b139249a9b705b1d0529c5c53cc8dca1265b7a17
Jacky Xie <jackyxie2520@outlook.com>
2023-08-15 00:13:20 -0400

    Emit structs and enums

--- include/codegen_c.hpp
+++ include/codegen_c.hpp
@@ -2,6 +2,8 @@
 
 #include "codegen.hpp"
 
+#include "fmt/core.h"
+
 namespace cinnabar {
   
 struct CTypeInfo {
@@ -15,12 +17,23 @@ class CodegenC {
 
   std::vector<CTypeInfo> ctypes;
   std::unordered_map<std::string_view, int> type_map;
+  
+  std::unordered_set<std::string> used_names; // easier to have owning
 
   std::string out;
+
+  // takes valid identifier or Generic type, mangles it
+  // Map[i32,Ref[i32]] -> "Map_i32_Ref_i32__4" ...
+  std::string mangle_name(std::string_view name);
   
-  void add_include(std::string_view file_name);
-  
-  void generate_types();
+  template<typename... Args>
+  void emit_line(fmt::format_string<Args...> fmt, Args&&... args);
+  void emit_include(std::string_view file_name);
+  void emit_types();
+  void emit_type_forward_declare(TypeId i);
+  // check type before calling
+  void emit_enum_definition(TypeId i);
+  void emit_struct_definition(TypeId i);
 
   [[noreturn]] void error(std::string_view message);
 
--- src/codegen_c.cpp
+++ src/codegen_c.cpp
@@ -1,27 +1,55 @@
 #include "codegen_c.hpp"
 
 #include <functional>
-#include "fmt/core.h"
 
 namespace cinnabar {
 
 CodegenC::CodegenC(const AST &ast) : ast(ast) {}
 
-void CodegenC::add_include(std::string_view file_name) {
-  out += fmt::format("#include {}\n", file_name);
+std::string CodegenC::mangle_name(std::string_view name) {
+  std::string res;
+  for (char c : name) {
+    if (c == '_' || isalnum(c)) {
+      res += c;
+    } else {
+      res += '_';
+    }
+  }
+  if (!used_names.contains(res)) {
+    used_names.insert(res);
+    return res;
+  }
+  for (int i = 0;; i++) {
+    std::string res_concat = res + std::to_string(i);
+    if (!used_names.contains(res_concat)) {
+      used_names.insert(res_concat);
+      return res_concat;
+    }
+  }
 }
 
-void CodegenC::generate_types() {
+template <typename... Args>
+void CodegenC::emit_line(fmt::format_string<Args...> fmt, Args &&...args) {
+  out += fmt::vformat(fmt, fmt::make_format_args(std::forward<Args>(args)...));
+  out += '\n';
+}
+
+void CodegenC::emit_include(std::string_view file_name) {
+  emit_line("#include {}", file_name);
+}
+
+void CodegenC::emit_types() {
   int num_types = ast.types.size();
   ctypes.reserve(num_types);
 
   std::vector<std::pair<const char *, const char *>> builtin_type_map = {
-      {"__fun", ""},      {"unit", ""},        {"i8", "int8_t"},
-      {"i16", "int16_t"}, {"i32", "int32_t"},  {"i64", "int64_t"},
-      {"u8", "uint8_t"},  {"u16", "uint16_t"}, {"u32", "u64"},
-      {"f32", "float"},   {"f64", "double"},   {"char", "char"},
-      {"bool", "bool"},   {"Ref", ""},         {"VarRef", ""},
-      {"Span", ""},       {"VarSpan", ""},     {"Span[char]", ""},
+      {"__fun", ""},       {"unit", ""},        {"i8", "int8_t"},
+      {"i16", "int16_t"},  {"i32", "int32_t"},  {"i64", "int64_t"},
+      {"u8", "uint8_t"},   {"u16", "uint16_t"}, {"u32", "uint32_t"},
+      {"u64", "uint64_t"}, {"f32", "float"},    {"f64", "double"},
+      {"char", "char"},    {"bool", "bool"},    {"Ref", ""},
+      {"VarRef", ""},      {"Span", ""},        {"VarSpan", ""},
+      {"Span[char]", ""},
   };
   for (size_t i = 0; i < builtin_type_map.size(); i++) {
     ctypes.emplace_back(builtin_type_map[i].second);
@@ -31,6 +59,7 @@ void CodegenC::generate_types() {
   std::vector<int> dependencies(num_types, 0);
   std::vector<std::vector<int>> types_depending_on(num_types); // adj list
 
+  // create dependency list and mangled type names
   for (int i = 0; i < num_types; i++) {
     const Type &type = ast.types[i];
     // clang-format off
@@ -45,6 +74,10 @@ void CodegenC::generate_types() {
           types_depending_on[type_id].push_back(i);
         }
         dependencies[i] = depend.size();
+
+        CTypeInfo ctype(mangle_name(decl->name.str));
+        ctypes.push_back(ctype);
+        emit_type_forward_declare(i);
       },
       [&](StructDecl *decl) {
         std::unordered_set<int> depend;
@@ -54,6 +87,10 @@ void CodegenC::generate_types() {
         for (int type_id : depend) {
           types_depending_on[type_id].push_back(i);
         }
+
+        CTypeInfo ctype(mangle_name(decl->name.str));
+        ctypes.push_back(ctype);
+        emit_type_forward_declare(i);
       },
     }, type.type_decl_ptr);
     // clang-format on
@@ -69,7 +106,9 @@ void CodegenC::generate_types() {
 
   std::function<void(TypeId)> dfs;
   dfs = [&](TypeId id) {
-    if (visit[id] == Status::DONE) return; // already in topo sort
+    if (visit[id] == Status::DONE) {
+      return; // already in topo sort
+    }
     if (visit[id] == Status::CURRENT_VISIT) { // loop detected
       std::string err_msg("Mutually recursive types: ");
       err_msg += ast.types[id].name(); // current
@@ -90,19 +129,73 @@ void CodegenC::generate_types() {
     }
     visit[id] = Status::DONE;
   };
-  
+
   for (size_t i = 0; i < ast.types.size(); i++) {
     dfs(i);
   }
   reverse(type_order.begin(), type_order.end());
+  
+  for (TypeId i : type_order) {
+    std::visit(overload{
+      [](BuiltinType *) {},
+      [&](EnumDecl *) { emit_enum_definition(i); },
+      [&](StructDecl *) { emit_struct_definition(i); },
+    }, ast.types[i].type_decl_ptr);
+  }
+}
+
+void CodegenC::emit_type_forward_declare(TypeId i) {
+  std::visit(
+      overload{
+          [&](BuiltinType *) {}, // skip
+          [&](EnumDecl *) {
+            emit_line("typedef struct {0} {0};", ctypes[i].mangled_name);
+          },
+          [&](StructDecl *) {
+            emit_line("typedef struct {0} {0};", ctypes[i].mangled_name);
+          },
+      },
+      ast.types[i].type_decl_ptr
+  );
+}
+
+void CodegenC::emit_enum_definition(TypeId i) {
+  const EnumDecl &decl = *std::get<EnumDecl *>(ast.types[i].type_decl_ptr);
+  emit_line("struct {} {{", ctypes[i].mangled_name);
+  // emit enum of variant
+  emit_line("  enum {{");
+  for (const auto &variant : decl.variants) {
+    emit_line("    __{}__enum_var__{},", ctypes[i].mangled_name, variant.name.str);
+  }
+  emit_line("  }}");
+  // emit union of variant
+  emit_line("  union {{");
+  for (const auto &variant : decl.variants) {
+    if (variant.type == type_map["unit"]) {
+      //emit_line("    {} {};", variant.name.str);
+    }
+    else {
+      emit_line("    {} {};", ctypes[variant.type].mangled_name, variant.name.str);
+    }
+  }
+  emit_line("  }}");
+  emit_line("}}", ctypes[i].mangled_name);
+}
+
+void CodegenC::emit_struct_definition(TypeId i) {
+  const StructDecl &decl = *std::get<StructDecl *>(ast.types[i].type_decl_ptr);
+  emit_line("struct {} {{", ctypes[i].mangled_name);
+  for (const auto &field : decl.fields) {
+    emit_line("  {} {};", ctypes[field.type].mangled_name, field.name.str);
+  }
+  emit_line("}}", ctypes[i].mangled_name);
 }
 
 std::string CodegenC::generate() {
   out = "";
-  add_include("<stdint.h>");
-  (void)ast;
+  emit_include("<stdint.h>");
+  emit_types();
 
-  generate_types();
   return out;
 }
 
